8a9b5c11266f953e087e61c78d21c3987a097793
Haridas Nishitha <nharidas@nvidia.com>
2026-06-09 16:48:30 +0530

    feat(te): Added unit test cases and test script for cardinal. Implemented 21-99 for telugu numericals
    Signed-off-by: Haridas Nishitha <nharidas@nvidia.com>

--- nemo_text_processing/text_normalization/te/taggers/cardinal.py
+++ nemo_text_processing/text_normalization/te/taggers/cardinal.py
@@ -24,6 +24,7 @@ class CardinalFst(GraphFst):
     Classifies cardinal numbers, e.g.  5  ->  cardinal { integer: "ఐదు" } 
     For compound numbers (21-99), it handles the pattern:  <tens> <units>, e.g.  42 ->  cardinal { integer: "నలభై రెండు" }
     For telugu digits, it uses the following mapping: e.g. ౧ -> cardinal { integer: "ఒకటి" }
+    including telugu numerals
     """ 
  
     def __init__(self, deterministic: bool = True): 
@@ -33,20 +34,20 @@ class CardinalFst(GraphFst):
         digit = pynini.string_file(get_abs_path("data/numbers/digit.tsv")) 
         zero = pynini.string_file(get_abs_path("data/numbers/zero.tsv")) 
         teens_and_ties = pynini.string_file(get_abs_path("data/numbers/teens_and_ties.tsv"))
-        tens = pynini.string_file(get_abs_path("/data/numbers/tens.tsv"))
+        tens = pynini.string_file(get_abs_path("data/numbers/tens.tsv"))
 
         # Predefined numbers (0-9, 10, 11 - 19, 20, 30, 40, 50, 60, 70, 80, 90)
         predefined = digit | zero | teens_and_ties
 
         # Two-digit compound (21-29, 31-39,...91-99)
         # For tens digit
-        first_digit = pynini.union("2", "3", "4", "5", "6", "7", "8", "9")
+        first_digit = pynini.union("2", "3", "4", "5", "6", "7", "8", "9", "౨", "౩", "౪", "౫", "౬", "౭", "౮", "౯")
     
         # Convert the tens value to tens word using teens_and_ties 
         tens_words = first_digit @ tens
 
         # For units digit
-        unit_digit = pynini.union("1", "2", "3", "4", "5", "6", "7", "8", "9")
+        unit_digit = pynini.union("1", "2", "3", "4", "5", "6", "7", "8", "9", "౧", "౨", "౩", "౪", "౫", "౬", "౭", "౮", "౯")
 
         # Convert the unit digit to unit word using predefined
         unit_words = unit_digit @ predefined
